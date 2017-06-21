cd4ebc5337f903fd685a3ea35094a099a4dbbecf
Tony <burkun.anton@gmail.com>
2017-06-21 17:14:27 +0300

    for test

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express = require('express');
-
-// Create our app
-var app = express();
-const PORT = process.env.PORT || 300;
-
-app.use(express.static('public'));
-
-app.use(function(req, res, next){
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
-        res.redirect('http://' + req.hostname + req.url);
-    }
-});
-
-app.listen(PORT, function () {
-  console.log('Express server is up on port 3000' + PORT);
-});
+// var express = require('express');
+//
+// // Create our app
+// var app = express();
+// const PORT = process.env.PORT || 300;
+//
+// app.use(express.static('public'));
+//
+// app.use(function(req, res, next){
+//     if (req.headers['x-forwarded-proto'] === 'http') {
+//         next();
+//     } else {
+//         res.redirect('http://' + req.hostname + req.url);
+//     }
+// });
+//
+// app.listen(PORT, function () {
+//   console.log('Express server is up on port 3000' + PORT);
+// });
