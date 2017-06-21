1ebe9a18615634b9468d3de0877a19857e9bb84f
Tony <burkun.anton@gmail.com>
2017-06-21 18:03:19 +0300

    for test

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 300;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+
 
 app.use(function(req, res, next){
     if (req.headers['x-forwarded-proto'] === 'https') {
@@ -13,6 +13,8 @@ app.use(function(req, res, next){
         next();
     }
 });
+
+app.use(express.static('public'));
 
 app.listen(PORT, function () {
   console.log('Express server is up on port 3000' + PORT);
