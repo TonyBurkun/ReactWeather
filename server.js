f1073411b9aaf9ee5352c2ad31c804f8bc78e40c
Tony <burkun.anton@gmail.com>
2017-06-21 17:17:13 +0300

    should work

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 300;
 
-// app.use(express.static('public'));
-//
-// app.use(function(req, res, next){
-//     if (req.headers['x-forwarded-proto'] === 'http') {
-//         next();
-//     } else {
-//         res.redirect('http://' + req.hostname + req.url);
-//     }
-// });
+app.use(express.static('public'));
+
+app.use(function(req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.listen(PORT, function () {
   console.log('Express server is up on port 3000' + PORT);
