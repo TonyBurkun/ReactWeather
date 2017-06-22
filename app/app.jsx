0413b6abca381a8a0435c8fc8df4694e3d357e9c
Tony <burkun.anton@gmail.com>
2017-06-22 10:58:21 +0300

    add foundation & jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,13 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
-var Weather = require('Weather');
-var Examples = require('Examples');
-var About = require('About');
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    {Route, Router, IndexRoute, hashHistory} = require('react-router'),
+    Main = require('Main'),
+    Weather = require('Weather'),
+    Examples = require('Examples'),
+    About = require('About');
+
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
 
 ReactDOM.render(
     <Router history={hashHistory}>
