df78de5ac0f14f4860b18f9bb5428c2a2fb85e8a
Tony <burkun.anton@gmail.com>
2017-06-22 12:07:26 +0300

    add some UI changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
-var React = require('react');
+var React = require('react'),
+    {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //     render: function () {
@@ -11,8 +12,17 @@ var React = require('react');
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples component</h3>
-            <p>Welcome to examples page</p>
+            <h1 className="text-center">Examples component</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=London">London</Link>
+
+                </li>
+                <li>
+                    <Link to="/?location=Rio">Rio</Link>
+                </li>
+            </ol>
         </div>
 
     );
